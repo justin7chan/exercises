7d745a5c7e7f8c238b8c254395924bd5d25aba76
Joel Ross <joelross@uw.edu>
2018-06-04 20:48:21 -0700

    git subrepo pull ch11-apis
    subrepo:
      subdir:   "ch11-apis"
      merged:   "788496b"
    upstream:
      origin:   "https://github.com/info201/ch11-apis"
      branch:   "master"
      commit:   "788496b"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

--- ch11-apis/exercise-1/exercise.R
+++ ch11-apis/exercise-1/exercise.R
@@ -4,52 +4,52 @@
 # You can also load `dplyr` if you wish to use it
 
 
-# Create a variable for the API's base URI (https://api.github.com)
+# Create a variable base_uri that stores the base URI (as a string) for the 
+# Github API (https://api.github.com)
 
 
-# Under the "Repositories" category, find the endpoint that will list repos in 
-# an organization
-# Create a variable `resource` that represents the endpoint for the book
-# organization (you can use `paste0()` to construct this, or enter it manually)
+# Under the "Repositories" category of the API documentation, find the endpoint 
+# that will list _repos in an organization_. Then create a variable named
+# `org_resource` that stores the endpoint for the `info201` organization repos 
+# (this is the _path_ to the resource of interest).
 
 
-# Send a GET request to this endpoint (the base.uri followed by the resource)
-# and extract the response body
+# Send a GET request to this endpoint (the `base_uri` followed by the 
+# `org_resource` path). Print the response to show that your request worked. 
+# (The listed URI will also allow you to inspect the JSON in the browser easily).
 
 
-# Convert the body from JSON into a data frame
+# Extract the content of the response using the `content()` function, saving it
+# in a variable.
+
+
+# Convert the content variable from a JSON string into a data frame.
 
 
 # How many (public) repositories does the organization have?
 
 
-
-# Use a "Search" endpoint to search for repositories about "visualization" whose
-# language includes "R"
-# Reassign the `resource` variable to refer to the appropriate resource.
-
-
-# You will need to specify some query parameters. Create a `query_params` list 
-# variable that specifies an appropriate key and value for the search term and
-# the language
+# Now a second query:
+# Create a variable `search_endpoint` that stores the endpoint used to search 
+# for repositories. (Hint: look for a "Search" endpoint in the documentation).
 
 
-# Send a GET request to this endpoint--including your params list as the `query`.
-# Extract the response body and convert it from JSON.
+# Search queries require a query parameter (for what to search for). Create a 
+# `query_params` list variable that specifies an appropriate key and value for 
+# the search term
 
 
-# How many search repos did your search find? (Hint: check the list names)
+# Send a GET request to the `search_endpoint`--including your params list as the
+# `query`. Print the response to show that your request worked.
 
 
-# What are the full names of the top 5 results?
+# Extract the content of the response and convert it from a JSON string into a
+# data frame. 
 
 
+# How many search repos did your search find? (Hint: check the list names to 
+# find an appropriate value).
 
-# Use the API to determine the number of people following Hadly Wickham 
-# (`hadley`, the author of dplyr, ggplot2, and other libraries we'll be using). 
 
-# Find an appropriate endpoint to query for statistics about a particular repo, 
-# and use it to get a list of contributors to the `tidyverse/dplyr` repository.
-# Who were the top 10 contributor in terms of number of total commits?
-# NOTE: This will be a really big response with lots of data!
+# What are the full names of the top 5 repos in the search results?
 
